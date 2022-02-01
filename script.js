7d274e3a9f8a1af7da28c78da4f754af8a10a905
Mnd2 <91032676+Mnd2@users.noreply.github.com>
2022-02-02 01:02:20 +0200

    swiper

--- script.js
+++ script.js
@@ -53,62 +53,6 @@ for (var tabElement of tabNavigationElements) {
     });
 }
 
-//
-// prices section swiper gallery
-//
-
-var pricing = new Swiper(".pricing-swiper", {
-    speed: 2000,
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: false,
-    autoplay: {
-        delay: 2000,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        1100: {
-            slidesPerView: 3,
-            spaceBetween: 120,
-        },
-        700: {
-            slidesPerView: 2,
-            spaceBetween: 100,
-        },
-    },
-});
-
-//
-// Reviews section swiper gallery
-//
-
-var swiper = new Swiper(".reviews-swiper", {
-    speed: 2000,
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    autoplay: {
-        delay: 2000,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        1100: {
-            slidesPerView: 3,
-            spaceBetween: 120,
-        },
-        700: {
-            slidesPerView: 2,
-            spaceBetween: 100,
-        },
-    },
-});
-
 //FormTop 
 var vardas = document.getElementById("first-name");
 var pavarde = document.getElementById("last-name");
