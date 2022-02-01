8456a666217a3aa78970e22b5c3c54d7d76d7e92
Mnd2 <91032676+Mnd2@users.noreply.github.com>
2022-02-01 22:19:24 +0200

    script

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 
 // Hamburger nav
 var btn = document.querySelector(".fancy-burger");
+var links = document.getElementsByClassName("link");
+var list = document.getElementById("hamburger-list");
+
 
 btn.addEventListener("click", () => {
     btn.querySelectorAll("span").forEach((span) => span.classList.toggle("open"));
+});
+
+for (var x of links) {
+    x.addEventListener("click", () => {
+        for (var x of links) {
+            x.classList.remove("active");
+        }
+        this.classList.add("active");
+    });
+}
+window.addEventListener("resize", () => {
+    list.classList.remove("width");
+    btn.classList.remove("open");
+    for (var x of links) {
+        x.classList.remove("active");
+    }
 });
 
 //
